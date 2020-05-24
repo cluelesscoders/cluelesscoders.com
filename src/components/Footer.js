c600f797d50c5bde9283221230f4927dba5b2771
Kunal Mangaraj <mangaraj.kunal@gmail.com>
2020-05-24 21:51:56 +0200

    modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,42 +2,28 @@ import React from "react";
 
 import { rhythm } from "../utils/typography";
 
-const Footer = () => (
-  <footer
-    style={{
-      marginTop: rhythm(2.5),
-      paddingTop: rhythm(1),
-    }}
-  >
-    <div style={{ float: "right" }}>
-      <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
-        rss
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        marginTop: rhythm(2.5),
+        paddingTop: rhythm(1),
+      }}
+    >
+      <div style={{ float: "right" }}>
+        <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+          rss
+        </a>
+      </div>
+      <a
+        href="https://github.com/cluelesscoders"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        github
       </a>
-    </div>
-    <a
-      href="https://mobile.twitter.com/dan_abramov"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      twitter
-    </a>{" "}
-    &bull;{" "}
-    <a
-      href="https://github.com/gaearon"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      github
-    </a>{" "}
-    &bull;{" "}
-    <a
-      href="https://stackoverflow.com/users/458193/dan-abramov"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      stack overflow
-    </a>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
